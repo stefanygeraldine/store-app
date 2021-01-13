096fd6551fd8461f347d608219c84f882f452aad
stefany <stefany.carballo@n5now.com>
2021-01-13 00:18:21 -0300

    describe: when the users submit the forms without values

--- src/form/Form.js
+++ src/form/Form.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TextField from '@material-ui/core/TextField'
 import Select from '@material-ui/core/Select'
 import InputLabel from '@material-ui/core/InputLabel'
+import FormHelperText from '@material-ui/core/FormHelperText'
 import Button from '@material-ui/core/Button'
 
-const Form = () => (
+const Form = () => {
+  const [errorText, setErrorText] = useState({
+    name: '',
+    size: '',
+    type: '',
+  })
+
+  const handleSubmit = e =>{
+    e.preventDefault()
+    const {name, size, type} = e.target.elements
+    if(!name.value){
+      setErrorText((prevProps)=>({...prevProps, name:' the name is required'}))
+    }
+    if(!size.value){
+      setErrorText((prevProps)=>({...prevProps, size:' the size is required'}))
+    }
+    if(!type.value){
+      setErrorText((prevProps)=>({...prevProps, type:' the type is required'}))
+    }
+  }
+  return (
     <>
       <h1>Create Form</h1>
-      <form  noValidate autoComplete="off">
-        <TextField  label="name" id="name" />
-        <TextField  label="size" id="size" />
+      <form onSubmit={handleSubmit}  noValidate autoComplete="off">
+        <TextField  label="name" id="name" helperText={errorText.name}/>
+        <TextField  label="size" id="size" helperText={errorText.size}/>
         <InputLabel htmlFor="type">Type</InputLabel>
         <Select
           native
@@ -18,14 +39,19 @@ const Form = () => (
             {id: "type"}
           }
         >
+          <option value="">seleccione</option>
           <option value="electronic">electronic</option>
           <option value="furniture">furniture</option>
           <option value="clothing">clothing</option>
         </Select>
-        <Button>Submit</Button>
+        {errorText.type.length &&
+        <FormHelperText>{errorText.type}</FormHelperText>
+        }
+        <Button type="submit">Submit</Button>
       </form>
     </>
   )
+}
 
 
 export default Form
--- src/form/Form.test.js
+++ src/form/Form.test.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import {screen, render} from "@testing-library/react"
+import {screen, render, fireEvent} from "@testing-library/react"
 import Form from "./Form";
 
 describe('When the Form is mounted',  ()=> {
   beforeEach(()=>render(<Form/>))
 
   it('should be a create product from page',  ()=> {
-    //expect(screen.queryByText(/Create Form/i).toBeInTheDocument)
     expect(screen.getByRole('heading', {name: /Create Form/i}).toBeInTheDocument)
   });
 
@@ -25,4 +24,19 @@ describe('When the Form is mounted',  ()=> {
   it('should exist submit button',  ()=> {
     expect(screen.getByRole('button', {name:/submit/i})).toBeInTheDocument()
   });
-});
+})
+
+describe('when the users submit the forms without values', ()=>{
+  beforeEach(()=>render(<Form/>))
+  it('should be display validation',  ()=> {
+    expect(screen.queryByText(/the name is required/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/the size is required/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/the type is required/i)).not.toBeInTheDocument()
+
+    fireEvent.click(screen.getByRole('button', {name:/submit/i}))
+
+    expect(screen.queryByText(/the name is required/i)).toBeInTheDocument()
+    expect(screen.queryByText(/the size is required/i)).toBeInTheDocument()
+    expect(screen.queryByText(/the type is required/i)).toBeInTheDocument()
+  });
+})
